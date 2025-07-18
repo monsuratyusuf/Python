faaac53ad96e3edfa6d0947f57bf1502c4187260
monsuratyusuf <jokeemqueen1983@gmail.com>
2025-07-18 15:43:20 -0400

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,8 @@
+#Printing a Greeting Message
+print("Hello Welcome to Python Pragram")
+#Printing my Name
+print("My Name is Abubakre")
+#Printing my Age
+print("I am 13 years old")
+#Printing a complex equation
+print("The result of 3678900 + 187500 is", 3678900 + 187500)
